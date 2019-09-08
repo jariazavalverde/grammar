f624553ddcdfe853f22711caeb30d49d69d8dcf3
José Antonio Riaza Valverde <riazavalverde@gmail.com>
2019-09-08 19:53:27 +0200

    added functions for remove direct left-recursion

--- src/Data/Grammar.hs
+++ src/Data/Grammar.hs
@@ -4,18 +4,19 @@ module Data.Grammar(
     isRegularGrammar,
     getProductionsByLeftSymbols,
     removeDirectLeftRecursion,
+    removeDirectLeftRecursionFor,
     removeLeftRecursion
 ) where
 
 
 import Data.Symbol(Symbol(..))
 import Data.Production(Production(..), isDirectlyLeftRecursive)
-import Data.List(intersperse, isInfixOf)
+import Data.List(nub, intersperse, isInfixOf, partition)
 
 
 data Grammar = Grammar {
-    getTerminalSymbols :: [Symbol],
     getNonTerminalSymbols :: [Symbol],
+    getTerminalSymbols :: [Symbol],
     getProductions :: [Production],
     getStartSymbol :: Symbol
 } deriving Ord
@@ -49,8 +50,25 @@ getProductionsByLeftSymbols g s = filter ((== s).getLeftSymbols) (getProductions
 
 -- | Remove direct left-recursion of the grammar.
 removeDirectLeftRecursion :: Grammar -> Grammar
-removeDirectLeftRecursion = error "not implemented yet"
+removeDirectLeftRecursion g = foldl removeDirectLeftRecursionFor g symbols
+    where (leftrec, _) = partition isDirectlyLeftRecursive (getProductions g)
+          symbols = nub $ map (head.getLeftSymbols) leftrec
+
+-- | Remove direct left-recursion of the grammar for a non-terminal symbol.
+removeDirectLeftRecursionFor :: Grammar -> Symbol -> Grammar
+removeDirectLeftRecursionFor _ (Terminal _) = error "non-terminal symbol expected"
+removeDirectLeftRecursionFor g@(Grammar n e p i) s = Grammar (s' : n) e (a ++ a' ++ p') i
+    where (leftrec, nonrec) = span isDirectlyLeftRecursive (getProductionsByLeftSymbols g [s])
+          s' = renameNonTerminalSymbol g s
+          a = map ((Production [s]).(++ [s']).getRightSymbols) nonrec
+          a' =  map ((Production [s']).(++ [s']).tail.getRightSymbols) leftrec ++ [Production [s'] []]
+          p' = filter ((/= [s]).getLeftSymbols) p
 
 -- | Remove all left-recursion of the grammar.
 removeLeftRecursion :: Grammar -> Grammar
 removeLeftRecursion = error "not implemented yet"
+
+-- | Rename a non-terminal symbol.
+renameNonTerminalSymbol :: Grammar -> Symbol -> Symbol
+renameNonTerminalSymbol _ (Terminal _) = error "non-terminal symbol expected"
+renameNonTerminalSymbol (Grammar n _ _ _) (NonTerminal x) = NonTerminal $ until (not.(`elem` n).NonTerminal) (++ "'") x
--- src/sample.hs
+++ src/sample.hs
@@ -9,12 +9,35 @@ import Data.ShortGrammar
 --   <B> → "b" <B> | ɛ
 g1 :: Grammar
 g1 = Grammar
-    [Terminal "a", Terminal "b"]
     [NonTerminal "A", NonTerminal "B"]
+    [Terminal "a", Terminal "b"]
     [
         Production [NonTerminal "A"] [Terminal "a", NonTerminal "A"],
         Production [NonTerminal "A"] [Terminal "b", NonTerminal "B"],
         Production [NonTerminal "B"] [Terminal "b", NonTerminal "B"],
         Production [NonTerminal "B"] []
+    ]
+    (NonTerminal "A")
+
+
+-- | Example 2: directly left-recursive grammar
+--   <A> → <A> "a1" <B> | <A> "a2" | "a3" | "a4"
+--   <B> → <B> "b1" <C> | <B> "b2" | "b3" | "b4"
+--   <C> → "c" <C> | ɛ
+g2 :: Grammar
+g2 = Grammar
+    [NonTerminal "A", NonTerminal "B", NonTerminal "C"]
+    [Terminal "a1", Terminal "a2", Terminal "a3", Terminal "a4"]
+    [
+        Production [NonTerminal "A"] [NonTerminal "A", Terminal "a1", NonTerminal "B"],
+        Production [NonTerminal "A"] [NonTerminal "A", Terminal "a2"],
+        Production [NonTerminal "A"] [Terminal "a3"],
+        Production [NonTerminal "A"] [Terminal "a4"],
+        Production [NonTerminal "B"] [NonTerminal "B", Terminal "b1", NonTerminal "C"],
+        Production [NonTerminal "B"] [NonTerminal "B", Terminal "b2"],
+        Production [NonTerminal "B"] [Terminal "b3"],
+        Production [NonTerminal "B"] [Terminal "b4"],
+        Production [NonTerminal "C"] [Terminal "c", NonTerminal "C"],
+        Production [NonTerminal "C"] []
     ]
     (NonTerminal "A")
